e1bf39e5684e8dbd26d3877673f34cd56c9af6c2
JitenSabharwal <jitensabharwal08@gmail.com>
2018-12-18 00:51:59 +0530

    fixed server deployment issue

--- bin/app.js
+++ bin/app.js
@@ -1,5 +1,5 @@
 const server = require('../server/server')
-const port = 8000
+const port = process.env.PORT || 8000
 const app = server()
 app.listen(port, () => {
   console.log('running at localhost: ' + port)
