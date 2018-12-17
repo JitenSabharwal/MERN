69f81a1fa086b160576569ba0b46738c83f0125f
JitenSabharwal <jitensabharwal08@gmail.com>
2018-12-18 01:14:24 +0530

    build added from heroku deployment

---
+++ client/build/precache-manifest.9304210b218348a348c0f7d6af19af0f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2fc326058287fac7e15",
+    "url": "/static/js/main.e2fc3260.chunk.js"
+  },
+  {
+    "revision": "65fcf66df934aa1bb86d",
+    "url": "/static/js/1.65fcf66d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d6aa5bc0fc517e1d05bd5878a4c499b8",
+    "url": "/index.html"
+  }
+];
